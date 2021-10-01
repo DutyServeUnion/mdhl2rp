3b0f4b7a3d3833b3f238608351b17bf28e072d10
DutyServeUnion <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-01 15:48:59 +0400

    Suppressing stuff

--- mdhl2rp/gamemode/cl_init.lua
+++ mdhl2rp/gamemode/cl_init.lua
@@ -1,2 +1,9 @@
 include("shared.lua")
 include("hud.lua")
+
+Hook.Add("OnPlayerChat", "UNIQUEIDENT" , function( ply, atr , bteam, bdead))
+
+  if (str = "/print") then
+    print("HEYYYY")
+    return true
+  end 
