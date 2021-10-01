ce337d32ce041091e56a0f146975f44ee8e1d8e2
Snappy <dobrin_k1@abv.bg>
2021-10-01 16:30:12 +0300

    F4Menu new color

--- mdhl2rp/gamemode/f4menu.lua
+++ mdhl2rp/gamemode/f4menu.lua
@@ -12,6 +12,9 @@ net.Receive("F4Menu", function()
         Menu.Paint = function()
             surface.SetDrawColor(60, 60, 60, 255)
             surface.DrawRect(0, 0, Menu:GetWide(), Menu:GetTall())
+
+            surface.SetDrawColor(40, 40, 40, 255)
+            surface.DrawRect(0,24,Menu:GetWide(), 1)
         end
     end
 
