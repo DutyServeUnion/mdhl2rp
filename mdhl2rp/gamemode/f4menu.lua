ef58f36c8d24df60da2eb85f422e313e06ace09f
Snappy <dobrin_k1@abv.bg>
2021-10-01 16:20:07 +0300

    F4Menu

--- mdhl2rp/gamemode/f4menu.lua
+++ mdhl2rp/gamemode/f4menu.lua
@@ -1,9 +1,21 @@
+local Menu
+
 net.Receive("F4Menu", function()
-    local Menu = vgui.Create("DFrame")
-    Menu:SetSize(500,500)
-    Menu:SetPos(ScrW() / 2 - 250, ScrH() / 2 - 250)
-    Menu:SetTitle("Menu")
-    Menu:SetDraggable(false)
-    Menu:ShowCloseButton(true)
-    Menu:MakePopup(true)
+    if (Menu == nil) then
+        Menu = vgui.Create("DFrame")
+        Menu:SetSize(500,500)
+        Menu:SetPos(ScrW() / 2 - 250, ScrH() / 2 - 250)
+        Menu:SetTitle("Roleplay Menu")
+        Menu:SetDraggable(true)
+        Menu:ShowCloseButton(false)
+        Menu:SetDeleteOnClose(false)
+    end
+
+    if (net.ReadBit() == 0) then
+        Menu:Hide()
+        gui.EnableScreenClicker(false)
+    else
+        Menu:Show()
+        gui.EnableScreenClicker(true)
+    end
 end)
--- mdhl2rp/gamemode/init.lua
+++ mdhl2rp/gamemode/init.lua
@@ -4,6 +4,8 @@ AddCSLuaFile("hud.lua")
 
 include("shared.lua")
 
+local open = false
+
 function GM:PlayerSpawn(ply)
     ply:SetMaxHealth(100)
     ply:Give("weapon_physcannon")
@@ -51,6 +53,13 @@ end
 
 util.AddNetworkString("F4Menu")
 function GM:ShowSpare2(ply)
+   if (open == false) then
+       open = true
+   else
+    open = false 
+   end
+   
     net.Start("F4Menu")
+    net.WriteBit(open)
     net.Broadcast()
 end
